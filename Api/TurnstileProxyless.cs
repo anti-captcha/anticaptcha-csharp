f26091f76144c69446c3e23218859a66f455bfbd
Anti Admin <robot@antigate.com>
2022-11-02 14:59:49 +0700

    Added Turnstile support

---
+++ Api/TurnstileProxyless.cs
@@ -0,0 +1,27 @@
+﻿using Anticaptcha_example.ApiResponse;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Anticaptcha_example.Api
+{
+    public class TurnstileProxyless : AnticaptchaBase, IAnticaptchaTaskProtocol
+    {
+        public Uri WebsiteUrl { protected get; set; }
+        public string WebsiteKey { protected get; set; }
+
+        public override JObject GetPostData()
+        {
+            return new JObject
+            {
+                {"type", "TurnstileTaskProxyless"},
+                {"websiteURL", WebsiteUrl},
+                {"websiteKey", WebsiteKey}
+            };
+        }
+
+        public TaskResultResponse.SolutionData GetTaskSolution()
+        {
+            return TaskInfo.Solution;
+        }
+    }
+}
