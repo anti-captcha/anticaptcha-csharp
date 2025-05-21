ab507b26494e2d9c6bef443b71d6f42ff52fef9e
Anti Admin <noreply@anti-captcha.com>
2025-05-21 16:53:05 +0300

    Amazon WAF support

---
+++ Api/AmazonProxyless.cs
@@ -0,0 +1,35 @@
+﻿using Anticaptcha_example.ApiResponse;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Anticaptcha_example.Api
+{
+    public class AmazonProxyless : AnticaptchaBase, IAnticaptchaTaskProtocol
+    {
+        public Uri WebsiteUrl { protected get; set; }
+        public string WebsiteKey { protected get; set; }
+        public string Iv { protected get; set; }
+        public string Context { protected get; set; }
+        public string CaptchaScript { protected get; set; }
+        public string ChallengeScript { protected get; set; }
+
+        public override JObject GetPostData()
+        {
+            return new JObject
+            {
+                {"type", "AmazonTaskProxyless"},
+                {"websiteURL", WebsiteUrl},
+                {"websiteKey", WebsiteKey},
+                {"iv", Iv},
+                {"context", Context},
+                {"captchaScript", CaptchaScript},
+                {"challengeScript", ChallengeScript}
+            };
+        }
+
+        public TaskResultResponse.SolutionData GetTaskSolution()
+        {
+            return TaskInfo.Solution;
+        }
+    }
+}
