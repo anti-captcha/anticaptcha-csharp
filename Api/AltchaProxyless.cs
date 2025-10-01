61a808101fd22281d0ab32dc740c27e684bc824a
Anti Admin <noreply@anti-captcha.com>
2025-10-01 15:39:09 +0700

    Altcha captcha support

---
+++ Api/AltchaProxyless.cs
@@ -0,0 +1,27 @@
+﻿using Anticaptcha_example.ApiResponse;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Anticaptcha_example.Api
+{
+    public class AltchaProxyless : AnticaptchaBase, IAnticaptchaTaskProtocol
+    {
+        public Uri WebsiteUrl { protected get; set; }
+        public string ChallengeURL { protected get; set; }
+
+        public override JObject GetPostData()
+        {
+            return new JObject
+            {
+                {"type", "AltchaTaskProxyless"},
+                {"websiteURL", WebsiteUrl},
+                {"challengeURL", ChallengeURL}
+            };
+        }
+
+        public TaskResultResponse.SolutionData GetTaskSolution()
+        {
+            return TaskInfo.Solution;
+        }
+    }
+}
